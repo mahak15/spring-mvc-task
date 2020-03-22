09e7df2130d2ff36c87f04da3e273d9feac616ef
Mahak <48390414+mahak15@users.noreply.github.com>
2020-03-22 21:12:21 +0530

    Add files via upload

---
+++ Annotation-mvc/src/main/java/com/config/WebConfig.java
@@ -0,0 +1,13 @@
+package com.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "com.example")
+public class WebConfig {
+
+
+}
